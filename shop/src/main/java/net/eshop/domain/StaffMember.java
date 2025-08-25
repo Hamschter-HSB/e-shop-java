77c37b646c61f7be7a27c5bb83a801ed9d92c221
Maxi Goldmann <info@maxigoldmann.de>
2025-08-25 07:33:35 +0200

    Delete shop directory

--- shop/src/main/java/net/eshop/domain/StaffMember.java
+++
@@ -1,8 +0,0 @@
-package net.eshop.domain;
-
-public class StaffMember extends User {
-
-    public StaffMember(int number, String name, String password) {
-        super(number, name, password);
-    }
-}
--- shop/src/main/java/net/eshop/ui/viewmodel/ViewModelUtils.java
+++
@@ -1,8 +0,0 @@
-package net.eshop.ui.viewmodel;
-
-public final class ViewModelUtils {
-
-    public static boolean currentUserIsStaffMember() {
-        return "STAFF_MEMBER".equals(System.getProperty("CURRENT_USER"));
-    }
-}
